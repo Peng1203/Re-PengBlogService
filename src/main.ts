ea347dc96a70d986d32446d99aa1e6bd022e58df
Peng <zhipeng.ma@pbn.com.cn>
2023-08-28 10:42:52 +0800

    chore: 项目启动并设置环境变量

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => console.log(`server is running: http://127.0.0.1:3000  --${process.env.NODE_ENV}`));
 }
 bootstrap();
