e79614f3f5d266520697930c8e79bd3d2f26acaa
Peng <zhipeng.ma@pbn.com.cn>
2023-08-28 14:59:11 +0800

    feat: 创建全局Dto层校验管道

---
+++ src/common/pipe/dto-validate.pipe.ts
@@ -0,0 +1,60 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+  ValidationPipe,
+} from '@nestjs/common';
+import { validate } from '@nestjs/class-validator';
+import { plainToClass } from '@nestjs/class-transformer';
+import { Reflector } from '@nestjs/core';
+
+// 转换参数列表
+const convertProps: string[] = ['page', 'pageSize', 'roleId'];
+
+const options = {
+  whitelist: true,
+  forbidNonWhitelisted: true,
+};
+// ValidationPipe 为内置校验管道
+@Injectable()
+export class DtoValidatePipe implements PipeTransform {
+  constructor() {}
+
+  async transform(value: any, metadata: ArgumentMetadata) {
+    // 转换部分query参数
+    const { type, metatype } = metadata;
+    // console.log('type ----->', type, metatype)
+    // 当没有设置 Dto 时跳过校验
+    if (!Object.getPrototypeOf(metatype).toString().includes('class'))
+      return value;
+    // 当触发 params 或者 custom 校验直接跳过 或 没有传递Dto校验数据直接返回
+    if (type === 'param' || type === 'custom' || !metatype) return value;
+    else if (type === 'query') {
+      // 当为query查询时 转换部分字段的 数据类型
+      for (const key in value) {
+        if (convertProps.indexOf(key) >= 0) value[key] = Number(value[key]);
+      }
+    }
+
+    const errors = await validate(plainToClass(metatype, value), options);
+    if (errors.length > 0) {
+      const errorMessage = this.flattenErrors(errors);
+      throw new BadRequestException(errorMessage);
+    }
+
+    return value;
+  }
+
+  private flattenErrors(errors: any[]): string {
+    return errors
+      .map((error) => {
+        for (const property in error.constraints) {
+          if (error.constraints.hasOwnProperty(property)) {
+            return error.constraints[property];
+          }
+        }
+      })
+      .join(', ');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { DtoValidatePipe } from './common/pipe/dto-validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const APP_HOST = configService.get<string>('APP_HOST')
-  const APP_PORT = configService.get<string>('APP_PORT')
+  const APP_HOST = configService.get<string>('APP_HOST');
+  const APP_PORT = configService.get<string>('APP_PORT');
 
-  app.useGlobalInterceptors(new TransformInterceptor())
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalPipes(new DtoValidatePipe());
 
-  await app.listen(APP_PORT, APP_HOST, () => console.log(`server is running: http://${APP_HOST}:${APP_PORT}  --${process.env.NODE_ENV}`));
+  await app.listen(APP_PORT, APP_HOST, () =>
+    console.log(
+      `server is running: http://${APP_HOST}:${APP_PORT}  --${process.env.NODE_ENV}`,
+    ),
+  );
 }
 bootstrap();
