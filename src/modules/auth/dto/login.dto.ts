5461c925bfcacb5da1c66d9c092c369e1cffb152
Peng1203 <2567810155@qq.com>
2023-08-31 18:38:20 +0800

    docs: swagger 登录接口默认参数

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -5,17 +5,17 @@ export class UserLoginDto {
   @IsString()
   @MinLength(2)
   @MaxLength(8)
-  @ApiProperty({ description: '用户名' })
+  @ApiProperty({ description: '用户名', default: 'admin' })
   userName: string;
 
   @IsString()
   @MinLength(6)
-  @ApiProperty({ description: '密码' })
+  @ApiProperty({ description: '密码', default: '123456' })
   password: string;
 
   @IsString()
   @MinLength(4)
   @MaxLength(4)
-  @ApiProperty({ description: '验证码' })
+  @ApiProperty({ description: '验证码', default: '1234' })
   captcha: string;
 }
