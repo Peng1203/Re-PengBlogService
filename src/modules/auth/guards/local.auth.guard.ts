a4fc0b4f2f5b731cba5e158a129705ab040274dd
Peng1203 <2567810155@qq.com>
2023-08-31 19:06:55 +0800

    feat(Auth): local策略守卫扩展

--- src/modules/auth/guards/local.auth.guard.ts
+++ src/modules/auth/guards/local.auth.guard.ts
@@ -1,7 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { PassPortStrategyEnum } from "@/helper/enums";
-import { AuthGuard } from "@nestjs/passport";
-
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { PassPortStrategyEnum } from '@/helper/enums';
+import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard(PassPortStrategyEnum.LOCAL) { }
+export class LocalAuthGuard extends AuthGuard(PassPortStrategyEnum.LOCAL) {
+  constructor() {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    console.log('执行了 localAuthGuard----->', req.body);
+    // 校验验证码
+
+    return this.activate(context);
+  }
+
+  async activate(context: ExecutionContext): Promise<boolean> {
+    return super.canActivate(context) as Promise<boolean>;
+  }
+}
