840fadf1b35a7151c9626e5155b7d378c253c7d7
Peng1203 <2567810155@qq.com>
2023-09-01 01:26:33 +0800

    feat: 扩展jwt策略

--- src/modules/auth/strategys/jwt.strategy.ts
+++ src/modules/auth/strategys/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
-
+import dayjs from '@/utils/date.util';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
@@ -14,10 +14,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     } as StrategyOptions);
   }
   // user 参数为 获取解析token的数据 return 内容 会作为 request 对象的 user属性存在
-  async validate(payload: any) {
+  async validate(payload: { sub: number; userName: string; iat: number; exp: number; iss: string }) {
     console.log('JWT策略触发 ----->', payload);
     // 通过 用户名和 id 查询数据库 redis 缓存优化
     // token 无感刷新
+    // 当前秒
+
+    const nowS = dayjs().unix();
+    console.log('token 剩余有效秒: ----->', payload.exp - nowS);
     //
     return 1212;
   }
