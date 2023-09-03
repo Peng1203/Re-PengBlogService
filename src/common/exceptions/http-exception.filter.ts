dbc7f3b74ca3cdb085ad5982534bd47c306441c6
Peng1203 <2567810155@qq.com>
2023-09-03 16:36:38 +0800

    refactor: http异常过滤器响应信息补充

--- src/common/exceptions/http-exception.filter.ts
+++ src/common/exceptions/http-exception.filter.ts
@@ -1,29 +1,25 @@
+import { ApiResponseMessageEnum } from '@/helper/enums';
 import { formatDate } from '@/utils/date.util';
-import {
-  ExceptionFilter,
-  Catch,
-  ArgumentsHost,
-  HttpException,
-} from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException & unknown, host: ArgumentsHost) {
-    const exceptionRes = exception.getResponse() as any
+    const exceptionRes = exception.getResponse() as any;
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
-    console.log('触发 Http 异常过滤器 ----->', exceptionRes, exceptionRes.response, exception.message)
+    console.log('触发 Http 异常过滤器 ----->', exceptionRes, exceptionRes.response, exception.message);
 
     res.status(status).json({
       code: exceptionRes.code || status,
       error: exception.message,
       path: req.url,
       method: req.method,
-      message: exceptionRes.msg,
+      message: exceptionRes.msg || ApiResponseMessageEnum[exceptionRes.code],
       timestamp: formatDate(),
     });
   }
