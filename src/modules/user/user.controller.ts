3a87d60e37f672ac81a08769ee29f8f4f12cbd25
Peng1203 <2567810155@qq.com>
2023-09-03 16:48:00 +0800

    test: 测试 Role 装饰器

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,7 +5,8 @@ import { UpdateUserDto } from './dto';
 import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import type { User } from './types';
 import { ListResponse } from '@/common/interface';
-import { Public } from '@/common/decorators';
+import { Public, Roles } from '@/common/decorators';
+import { RoleEnum } from '@/helper/enums';
 
 @ApiTags('User')
 @ApiBearerAuth()
@@ -19,7 +20,7 @@ export class UserController {
   }
 
   @Get()
-  @Public()
+  @Roles(RoleEnum.ADMINISTRATOR, RoleEnum.USER)
   @ApiOperation({ summary: '查询全部用户' })
   async findAll(@Query() query: FindAllUserDto): Promise<ListResponse<User>> {
     const { list: data, total } = await this.usersService.findAll(query);
