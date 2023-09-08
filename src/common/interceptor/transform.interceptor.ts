6ad14e218b70a4682939be620e54eb9fcca9754e
Peng1203 <2567810155@qq.com>
2023-09-08 18:54:08 +0800

    feat: 接口响应转换传入自定义message

--- src/common/interceptor/transform.interceptor.ts
+++ src/common/interceptor/transform.interceptor.ts
@@ -9,20 +9,24 @@ import { IS_KEEP_KEY } from '../decorators';
 export class TransformInterceptor implements NestInterceptor {
   constructor(private readonly reflector: Reflector) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const isKeep = this.reflector.getAllAndOverride<boolean>(IS_KEEP_KEY, [context.getHandler(), context.getClass()]);
+    const isKeep = this.reflector.getAllAndOverride<boolean>(IS_KEEP_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     return next.handle().pipe(
       map((data) => {
         if (isKeep) return data;
 
         const res = context.switchToHttp().getResponse<Response>();
-        const success = res.success ?? true;
         const code = res.apiResponseCode ?? ApiResponseCodeEnum.SUCCESS;
+        const message = res.resMsg || ApiResponseMessageEnum[code];
+        const success = res.success ?? true;
         // 当设置了响应头时 返回原始数据
         // if (res.getHeader('Content-Type')) return data;
         return {
           code,
           success,
-          message: ApiResponseMessageEnum[code],
+          message,
           data,
         };
       }),
