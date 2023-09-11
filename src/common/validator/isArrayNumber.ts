cef5efa3810d6ac7ebdeb588c513c1325936fc0f
Peng1203 <2567810155@qq.com>
2023-09-11 19:03:19 +0800

    feat: 创建校验数字数组的类

---
+++ src/common/validator/isArrayNumber.ts
@@ -0,0 +1,17 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from '@nestjs/class-validator';
+import { BadRequestException } from '@nestjs/common';
+
+@ValidatorConstraint({ name: 'custom', async: false })
+export class IsArrayNumber implements ValidatorConstraintInterface {
+  validate(value: number[], validationArguments?: ValidationArguments): boolean | Promise<boolean> {
+    return value.every((item) => typeof item === 'number');
+  }
+
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    throw new BadRequestException('数字类型的数组有误');
+  }
+}
