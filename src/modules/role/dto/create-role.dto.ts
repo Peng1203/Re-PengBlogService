26fad54f465b866691925f6c49713295f5f94995
Peng1203 <2567810155@qq.com>
2023-10-30 18:09:42 +0800

    feat(Role): role接口开发

--- src/modules/role/dto/create-role.dto.ts
+++ src/modules/role/dto/create-role.dto.ts
@@ -1,1 +1,24 @@
-export class CreateRoleDto {}
+import { IsArrayNumber } from '@/common/validator';
+import { IsNumber, IsOptional, IsString, MaxLength, MinLength } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRoleDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(8)
+  @ApiProperty({ description: '角色名称' })
+  readonly userName: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ description: '角色描述' })
+  readonly description?: string;
+
+  @IsNumber({}, { each: true })
+  @ApiProperty({ description: '角色菜单', default: [] })
+  readonly menus: number[];
+
+  @IsNumber({}, { each: true })
+  @ApiProperty({ description: '角色权限', default: [] })
+  readonly permissions: number[];
+}
--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -2,8 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestj
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FindAllRoleDto } from './dto';
+import { ParseIntParamPipe } from '@/common/pipe';
 
 @ApiTags('Role')
 @ApiBearerAuth()
@@ -12,18 +13,21 @@ export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.roleService.create(createRoleDto);
+  @ApiOperation({ summary: '创建角色' })
+  create(@Body() data: CreateRoleDto) {
+    return this.roleService.create(data);
   }
 
   @Get()
+  @ApiOperation({ summary: '查询角色' })
   findAll(@Query() query: FindAllRoleDto) {
     return this.roleService.findAll(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(+id);
+  @ApiOperation({ summary: '通过ID查询角色' })
+  findOne(@Param('id', new ParseIntParamPipe('id参数有误')) id: number) {
+    return this.roleService.findOne(id);
   }
 
   @Patch(':id')
