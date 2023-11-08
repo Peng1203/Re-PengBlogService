4d172fe2fe277083bfb6dbc84dc65bf960e9e6fd
Peng1203 <2567810155@qq.com>
2023-11-08 19:21:50 +0800

    update: 权限表更新

--- src/common/entities/permission.entity.ts
+++ src/common/entities/permission.entity.ts
@@ -1,10 +1,13 @@
 import { TimestampedEntity } from '@/common/entities';
-import { ActionTypeEnum } from '@/helper/enums';
+// import { ActionTypeEnum } from '@/helper/enums';
 import { Role } from './';
 import { Column, Entity, Index, ManyToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { RequestMethodEnum } from '@/helper/enums/request.method';
+import { PermissionEnum } from '@/helper/enums/permission';
 
 @Entity({ name: 'permission' })
-@Unique(['permissionName', 'permissionCode'])
+@Unique(['permissionName'])
+@Unique(['permissionCode'])
 export class Permission extends TimestampedEntity {
   @PrimaryGeneratedColumn()
   readonly id: number;
@@ -14,13 +17,33 @@ export class Permission extends TimestampedEntity {
   readonly permissionName: string;
 
   @Index('index_permission_code')
-  @Column({ name: 'permission_code', type: 'varchar', length: 20 })
-  readonly permissionCode: string;
+  @Column({
+    name: 'permission_code',
+    type: 'enum',
+    enum: PermissionEnum,
+    nullable: true,
+  })
+  readonly permissionCode: PermissionEnum | null;
 
-  @Column({ type: 'enum', enum: ActionTypeEnum })
-  readonly type: ActionTypeEnum;
+  // @Column({ type: 'enum', enum: ActionTypeEnum })
+  // readonly action_type: ActionTypeEnum;
 
-  @Column({ name: 'description', type: 'varchar', length: 60, nullable: true })
+  @Column({
+    name: 'resource_method',
+    type: 'enum',
+    enum: RequestMethodEnum,
+    // default: RequestMethodEnum.GET,
+    nullable: true,
+  })
+  readonly resourceMethod: RequestMethodEnum | null;
+
+  @Column({ name: 'resource_url', type: 'varchar', nullable: true })
+  readonly resourceUrl: string | null;
+
+  @Column({ name: 'parent_id', type: 'int', default: 0 })
+  readonly parentId: number;
+
+  @Column({ type: 'varchar', length: 60, nullable: true })
   readonly description: string;
 
   @ManyToMany(() => Role, (Role) => Role.permissions)
