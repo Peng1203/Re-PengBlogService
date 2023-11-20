4c06b02c36a878508277c7d489c346cc9b75ecc2
Peng1203 <2567810155@qq.com>
2023-11-20 19:26:08 +0800

    feat: 添加权限标识枚举

--- src/helper/enums/permission.ts
+++ src/helper/enums/permission.ts
@@ -1,8 +1,10 @@
 export enum PermissionEnum {
-  // 创建用户
+  // 用户
   CREATE_USER = 'create_user',
-  // 更新用户
   UPDATE_USER = 'update_user',
-  // 删除用户
   DELETE_USER = 'delete_user',
+
+  // 角色
+  CREATE_ROLE = 'create_role',
+  UPDATE_ROLE = 'update_role',
 }
