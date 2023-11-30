3d0596f6b763eb89a191e65cab304c02cad8c7ae
Peng1203 <2567810155@qq.com>
2023-11-30 15:53:15 +0800

    feat(Article): 发布文章接口开发

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,22 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ForbiddenException,
+} from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserService } from '@/modules/user/user.service';
+import { ReqUser } from '@/common/decorators';
+import { ApiResponseCodeEnum } from '@/helper/enums';
 
 @ApiTags('Article')
 @ApiBearerAuth()
 @Controller('article')
 export class ArticleController {
-  constructor(
-    private readonly articleService: ArticleService,
-    private readonly usersService: UserService,
-  ) {}
+  constructor(private readonly articleService: ArticleService) {}
 
   @Post()
-  async create(@Body() data: CreateArticleDto) {
-    await this.usersService.findOneById(data.authorId);
+  async create(@Body() data: CreateArticleDto, @ReqUser('id') uid: number) {
+    if (uid !== data.authorId)
+      throw new ForbiddenException({
+        code: ApiResponseCodeEnum.FORBIDDEN_USER,
+        msg: '身份信息有误！',
+      });
+
     return this.articleService.create(data);
   }
 
--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiResponseCodeEnum } from '@/helper/enums';
+import { TagService } from '@/modules/tag/tag.service';
+import { CategoryService } from './../category/category.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Article } from '@/common/entities';
+import { UserService } from '@/modules/user/user.service';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+  constructor(
+    @InjectRepository(Article) private readonly articleRepository: Repository<Article>,
+    private readonly tagService: TagService,
+    private readonly usersService: UserService,
+    private readonly categoryService: CategoryService,
+  ) {}
+
+  async create(data: CreateArticleDto) {
+    try {
+      const { category: categoryId, tags: tagIds, authorId, ...args } = data;
+
+      const author = await this.usersService.findOneById(authorId);
+
+      const tags = tagIds.length
+        ? (await Promise.all(tagIds.map((id) => this.tagService.findOne(id)))).filter((tag) => tag)
+        : [];
+      const category = categoryId ? await this.categoryService.findOne(categoryId) : null;
+
+      const article = await this.articleRepository.create({ author, tags, category, ...args });
+
+      return await this.articleRepository.save(article);
+    } catch (e) {
+      throw new InternalServerErrorException({
+        e,
+        code: ApiResponseCodeEnum.INTERNALSERVERERROR_SQL_CREATED,
+        msg: '发布文章失败',
+      });
+    }
   }
 
   findAll() {
