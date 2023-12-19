aa5ef9d023c12456f2d34109078a737d8c3eb191
Peng1203 <2567810155@qq.com>
2023-12-19 18:52:55 +0800

    feat(Log): 打印客户端真实IP

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@/utils/date.util';
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
@@ -5,11 +6,14 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const statusCode = res.statusCode;
+    const clientIp = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
     const logFormat = `
 ###########################################################
+Date: ${formatDate()}
 RequestUrl: ${req.originalUrl}
 Method: ${req.method}
 IP: ${req.ip}
+ClientIP: ${clientIp}
 StatusCode: ${statusCode}
 Params: ${JSON.stringify(req.params)}
 Query: ${JSON.stringify(req.query)}
@@ -17,6 +21,9 @@ Body: ${JSON.stringify(req.body)}
 ###########################################################
 `;
     Logger.log(logFormat);
+    this.writeLog(logFormat);
     next();
   }
+
+  private writeLog(logInfo: string) {}
 }
