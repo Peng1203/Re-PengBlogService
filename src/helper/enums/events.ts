6582c7a0650d47688ab3f141899aac0baaeb5f14
Peng1203 <2567810155@qq.com>
2023-12-24 23:10:38 +0800

    feat: 创建自定义事件枚举常量

---
+++ src/helper/enums/events.ts
@@ -0,0 +1,8 @@
+export enum EventsEnum {
+  /** 执行部分更新Web命令完成 操作触发的事件 */
+  UPDATE_WEB_MSG = 'update.web.msg',
+  /** 执行部分更新Admin命令完成 操作触发的事件 */
+  UPDATE_ADMIN_MSG = 'update.admin.msg',
+  /** 执行部分更新Serve命令完成 操作触发的事件 */
+  UPDATE_SERVE_MSG = 'update.serve.msg',
+}
