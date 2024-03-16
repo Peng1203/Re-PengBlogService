732a9e4a5ea88fc0eadc38d39fa44860d995b77b
Peng1203 <2567810155@qq.com>
2024-03-16 20:58:29 +0800

    feat(ChatGPT): :sparkles: 新增axios调用方式

--- src/modules/open-ai/open-ai.controller.ts
+++ src/modules/open-ai/open-ai.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Header, Post, Res } from '@nestjs/common';
 import { OpenAiService } from './open-ai.service';
-import { Public } from '@/common/decorators';
 import { ChatDto } from './dto';
 import { ProxyHttpService } from '@/shared/proxyHttp/proxyHttp.service';
 import { Response } from 'express';
@@ -13,32 +12,34 @@ export class OpenAiController {
   ) {}
 
   @Post('chat')
-  @Public()
-  // @Header('Content-Type', 'text/event-stream')
   // @Header('Content-Type', 'text/plain')
   async chartGpt(@Body() data: ChatDto, @Res() res: Response) {
-    // const { data: response } = await this.proxyHttpService.openAiHttp({
-    //   method: 'POST',
-    //   url: '/chat/completions',
-    //   data: {
-    //     model: 'gpt-3.5-turbo',
-    //     messages: [{ role: 'system', content: data.content }],
-    //     max_tokens: 1024,
-    //     temperature: 0.6,
-    //     stream: true,
-    //   },
-    //   responseType: 'stream',
-    // });
-    // response.pipe(res);
-
     const textDecoder = new TextDecoder();
-    const stream = this.openAiService.getStreamResponse(data);
+    const stream = await this.openAiService.getStreamResponse(data);
     for await (const chunk of (stream as any).toReadableStream()) {
       const data = textDecoder.decode(chunk);
       res.write(data);
     }
     res.end();
-
     // const stream = this.openAiService.getStreamResponse(data.content);
+  }
+
+  @Post('v2/chat')
+  // @Header('Content-Type', 'text/event-stream')
+  async chartGpt_v2(@Body() { messages }: ChatDto, @Res() res: Response) {
+    const { data: response } = await this.proxyHttpService.openAiHttp({
+      method: 'POST',
+      url: '/chat/completions',
+      data: {
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+        max_tokens: 1024,
+        temperature: 0.6,
+        stream: true,
+      },
+      responseType: 'stream',
+    });
+
+    response.pipe(res);
   }
 }
