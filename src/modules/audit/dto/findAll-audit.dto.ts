7fb8b1e28594705086cecd3d29f148e20f16146a
Peng1203 <2567810155@qq.com>
2024-03-21 19:21:26 +0800

    feat: 补充审计列表查日期参数

--- src/modules/audit/dto/findAll-audit.dto.ts
+++ src/modules/audit/dto/findAll-audit.dto.ts
@@ -1,5 +1,13 @@
 import { ListCommonParamsDto } from '@/common/dto';
-import { IsInt, IsNumber, IsOptional } from '@nestjs/class-validator';
+import { IsDateTimeString } from '@/helper/validate';
+import {
+  IsDefined,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Validate,
+} from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class FindAllAuditDto extends ListCommonParamsDto {
@@ -8,4 +16,18 @@ export class FindAllAuditDto extends ListCommonParamsDto {
   @IsOptional()
   @ApiProperty({ default: '', required: false, description: '操作用户ID' })
   readonly userId?: number;
+
+  @IsOptional()
+  @IsString()
+  @Validate(IsDateTimeString, { message: '开始时间格式有误' })
+  @IsDefined()
+  @ApiProperty({ default: '', required: false, description: '开始时间' })
+  readonly startTime?: string;
+
+  @IsOptional()
+  @IsString()
+  @Validate(IsDateTimeString, { message: '结束时间格式有误' })
+  @IsDefined()
+  @ApiProperty({ default: '', required: false, description: '结束时间' })
+  readonly endTime?: string;
 }
