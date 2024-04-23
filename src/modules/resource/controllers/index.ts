52e67c87104e8d7a1e3286a86d696a395a644d04
Peng <zhipeng.ma@pbn.com.cn>
2024-04-23 16:51:14 +0800

    pref: 去除无用代码

--- src/modules/resource/controllers/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './resource.netdisk.controller';
--- src/modules/resource/resource.module.ts
+++ src/modules/resource/resource.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ResourceService } from './resource.service';
 import { ResourceController } from './resource.controller';
 import { CommonModule } from '@/shared/common.module';
-import { NetDiskController } from './controllers';
 
 @Module({
   imports: [CommonModule],
-  controllers: [ResourceController, NetDiskController],
+  controllers: [ResourceController],
   providers: [ResourceService],
 })
 export class ResourceModule {}
