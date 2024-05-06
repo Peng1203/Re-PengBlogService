456eb75cf88fd0ca31b0e7d967796321be92bf46
Peng <zhipeng.ma@pbn.com.cn>
2024-05-06 16:01:26 +0800

    feat(Auth): 根据acToken获取用户信息

--- src/modules/user/decorator/uploadAvater.aggregation.decorator.ts
+++ src/modules/user/decorator/uploadAvater.aggregation.decorator.ts
@@ -1,14 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import mime from 'mime-types';
+import { nanoid } from 'nanoid';
+import { Request } from 'express';
+import { diskStorage } from 'multer';
 // import { Public } from '@/common/decorators';
 import { MethodNotAllowedException, UseInterceptors, applyDecorators } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiOperation } from '@nestjs/swagger';
 import { UpdateAvaterDto } from '../dto';
-import { diskStorage } from 'multer';
-import { Request } from 'express';
-import path from 'path';
-import { nanoid } from 'nanoid';
-import mime from 'mime-types';
 
+const mkdirAsync = promisify(fs.mkdir);
 /**
  * 上传头像装饰器聚合
  */
@@ -17,9 +20,13 @@ export function UploadAvaterAggregation() {
     UseInterceptors(
       FileInterceptor('file', {
         storage: diskStorage({
-          destination(req: Request, file, callback) {
+          async destination(req: Request, file, callback) {
             const { STATIC_RESOURCE_PATH } = process.env;
-            callback(null, path.resolve(STATIC_RESOURCE_PATH));
+            const uploadPath = path.resolve(STATIC_RESOURCE_PATH);
+            const hasDir = fs.existsSync(uploadPath);
+            if (!hasDir) await mkdirAsync(uploadPath);
+
+            callback(null, uploadPath);
           },
           filename(req: Request, file, callback) {
             const extname = mime.extension(file.mimetype);
