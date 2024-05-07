60311f3a3ec403b836b0cdfcc9cf7282372b688c
Peng <zhipeng.ma@pbn.com.cn>
2024-05-07 09:44:39 +0800

    build: 配置生产环境pm2配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'blog-serve-nest:3000',
-      script: 'dist/main.js',
+      name: 'blog_serve',
+      script: 'pnpm',
+      args: 'run start:prod',
       // instances: 'max', // 根据需求设置实例数量
-      exec_mode: 'cluster',
+      exec_mode: 'fork',
       autorestart: true,
-      watch: true,
+      watch: false,
       // max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'development',
-        // NODE_ENV: 'production',
+        // NODE_ENV: 'development',
+        NODE_ENV: 'production',
       },
     },
   ],
