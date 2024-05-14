8f96e400282e0ddb8e22c07e6f404a9b9163e4c0
Peng <zhipeng.ma@pbn.com.cn>
2024-05-14 19:08:37 +0800

    feat(Guard): 新增身份校验守卫

---
+++ src/common/guards/identity.guard.ts
@@ -0,0 +1,26 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { ApiResponseCodeEnum } from '@/helper/enums';
+
+/** 用于校验 执行敏感操作 是否越权 */
+@Injectable()
+export class IdentityGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const paramsId = Number(req.params.id || req.params.uid);
+    // prettier-ignore
+    if (paramsId != req.user.id) throw new ForbiddenException({
+      code: ApiResponseCodeEnum.FORBIDDEN_USER,
+      msg: `操作失败 身份信息有误！`,
+    });
+    return true;
+  }
+}
